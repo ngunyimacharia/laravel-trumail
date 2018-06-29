3f328b59aba204cdbe7a15dabfe91908ff92d95c
Kelvin Macharia <kelvin@farwell-consultants.com>
2018-06-29 15:17:40 +0300

    Config file added to project
    The configuration file allows setting the following values:
     - token
     - language
     - error responses

--- config/trumail.php
+++ config/trumail.php
@@ -1,5 +1,56 @@
 <?php
 
 return [
-  'token' => env('TRUMAIL_TOKEN'),
+
+  /*
+  |--------------------------------------------------------------------------
+  | Trumail Token
+  |--------------------------------------------------------------------------
+  |
+  | This is the token used to access the trumail.io api.
+  | To generate a token, create an account and login to https://trumail.io
+  | Copy the api token in the 'API Tokens' section
+  |
+  |
+  */
+
+  'token' => env('TRUMAIL_TOKEN',null),
+
+  /*
+  |--------------------------------------------------------------------------
+  | Trumail Language
+  |--------------------------------------------------------------------------
+  |
+  | This is the language to be used in rendering the trumail responses
+  | Currently we only support American English.
+  | You may propose a new language by contacting me directly or be contributing to this project
+  | You may also add your desired lang to the response configuration below
+  |
+  */
+
+  'lang' => env('TRUMAIL_LANG','en_US'),
+
+  /*
+  |--------------------------------------------------------------------------
+  | Trumail Responses
+  |--------------------------------------------------------------------------
+  |
+  | These are the response messages that will be returned an error has been detected
+  | Currently we only support American English.
+  | You may propose a new language by contacting me directly or be contributing to this project
+  | You may also add your desired lang to the response configuration below
+  |
+  */
+
+  'error_responses' => [
+
+    'en_US' => [
+      'format' => 'Invalid email format',
+      'delivery' => 'Email address not deliverable',
+      'inbox' => 'Email address inbox full',
+      'host' => 'Email address host/domain doesn\'t exist'
+    ]
+
+  ],
+
 ];
--- src/TrumailServiceProvider.php
+++ src/TrumailServiceProvider.php
@@ -7,41 +7,53 @@ use Illuminate\Support\ServiceProvider;
 class TrumailServiceProvider extends ServiceProvider
 {
 
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = false;
+  /**
+  * Indicates if loading of the provider is deferred.
+  *
+  * @var bool
+  */
+  protected $defer = false;
 
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-      //
+  /**
+  * Bootstrap the application services.
+  *
+  * @return void
+  */
+  public function boot()
+  {
+
+    // Copy config file to laravel project
+    $this->publishes([
+        __DIR__.'../config/trumail.php' => config_path('trumail.php'),
+    ]);
+
+  }
+
+  /**
+  * Register the application services.
+  *
+  * @return void
+  */
+  public function register()
+  {
+
+    //Set config file
+    if ($this->app['config']->get('trumail') === null) {
+      $this->app['config']->set('trumail', require __DIR__.'/../config/trumail.php');
     }
 
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->singleton('TrumailService', function ($app) {
-                return new TrumailService();
-        });
-    }
+    //Set service
+    $this->app->singleton('TrumailService', function ($app) {
+      return new TrumailService();
+    });
+  }
 
-    /**
-     * @return array
-     */
-    public function provides()
-    {
-        return array('Trumail');
-    }
+  /**
+  * @return array
+  */
+  public function provides()
+  {
+    return array('Trumail');
+  }
 
 }
